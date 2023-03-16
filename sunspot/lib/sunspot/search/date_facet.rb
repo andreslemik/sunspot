56f787b235f1ac0f9dc844fd6125e6dc03742967
Andrew Svidersky <andreslemik@yandex.ru>
2023-03-16 13:20:45 +0400

    fixes date_field_facet

--- sunspot/lib/sunspot/search/date_facet.rb
+++ sunspot/lib/sunspot/search/date_facet.rb
@@ -2,7 +2,9 @@ module Sunspot
   module Search
     class DateFacet
       def initialize(field, search, options)
-        @field, @search, @options = field, search, options
+        @field = field
+        @search = search
+        @options = options
       end
 
       def field_name
@@ -12,15 +14,16 @@ module Sunspot
       def rows
         @rows ||=
           begin
-            data = @search.facet_response['facet_dates'][@field.indexed_name]
-            gap = (@options[:time_interval] || 86400).to_i
+            data = @search.facet_response['facet_ranges'][@field.indexed_name]
+            gap = (@options[:time_interval] || 86_400).to_i
             rows = []
-            data.each_pair do |value, count|
-              if value =~ /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/
-                start_time = @field.cast(value)
-                end_time = start_time + gap
-                rows << FacetRow.new(start_time..end_time, count, self)
-              end
+            values = data['counts'].each_slice(2).map { |value| value }.to_h
+            values.each_pair do |value, count|
+              next unless value =~ /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/
+
+              start_time = @field.cast(value)
+              end_time = start_time + gap
+              rows << FacetRow.new(start_time..end_time, count, self)
             end
             if @options[:sort] == :count
               rows.sort! { |lrow, rrow| rrow.count <=> lrow.count }
